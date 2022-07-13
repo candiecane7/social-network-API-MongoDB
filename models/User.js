74a3d2e764f74d99b0637363332eb110e34ea332
Candice Hall <hall.candice@outlook.com>
2022-07-13 11:29:45 -0400

    added virtual to pizza schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,40 @@
-const {Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
+require('mongoose-type-email');
 
+//username and email, thoughts(other model) and friends(self-referencing) 
 const UserSchema = new Schema({
-    
+    username: {
+        type: String,
+        unique: true,
+        required: 'You need to provide a username',
+        trim: true
+    },
+    email: {
+        type: mongoose.SchemaTypes.Email,
+        required: 'You need to provide an email'
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: true
+}
+);
+
+UserSchema.virtual('friendCount').get(function(){
+    return this.friends.reduce((total, friends)=> total + friends.length + 1, 0);
 })
