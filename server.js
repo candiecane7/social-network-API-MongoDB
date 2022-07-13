6e5dcf6b845e8529ffb901322da2b784528a05e9
Candice Hall <hall.candice@outlook.com>
2022-07-13 10:47:10 -0400

    starting pizza schema

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
-app.use(require('./routes'));
+//  app.use(require('./routes'));
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 mongoose.set('debug', true);
 
