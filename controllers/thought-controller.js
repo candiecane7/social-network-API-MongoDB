9f3a88260ad2a580274f5db790a0d8d42ea6d25e
Candice Hall <hall.candice@outlook.com>
2022-07-13 12:12:07 -0400

    added initial set up for controllers

---
+++ controllers/thought-controller.js
@@ -0,0 +1,13 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    //get all thoughts
+    //get a single thought by id
+    //post a new thought
+    //put thought update by id
+    //delete thought
+
+    // /api/thoughts/:thoughtId/reactions
+    //post reaction
+    //delete reaction
+}
---
+++ controllers/user-controller.js
@@ -0,0 +1,15 @@
+const { User, Thought } = require("../models");
+
+const userController = {
+    //get all users
+    //get 1 user
+    // post new user
+    //put to update user by id
+    //delete user by id * bonus - remove thoughts when deleted
+
+    // /api/users/:userId/friends/:friendId
+    //post new friend to friends list
+    //delete a friend from friends list
+};
+
+module.exports = userController;
